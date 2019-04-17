63277a994be36f6d79ec6bea976d35065de3f80f
Kennnyshiwa <kstjohn@kstjohn.us>
2019-04-16 20:01:52 -0400

    update error counting in event.py

--- redbot/core/events.py
+++ redbot/core/events.py
@@ -130,7 +130,6 @@ def init_events(bot, cli_flags):
 
     @bot.event
     async def on_command_error(ctx, error, unhandled_by_cog=False):
-
         if not unhandled_by_cog:
             if hasattr(ctx.command, "on_error"):
                 return
@@ -173,6 +172,7 @@ def init_events(bot, cli_flags):
                 traceback.format_exception(type(error), error, error.__traceback__)
             )
             bot._last_exception = exception_log
+            bot.counter['cmd_error'] += 1
             await ctx.send(inline(message))
         elif isinstance(error, commands.CommandNotFound):
             fuzzy_commands = await fuzzy_command_search(
@@ -215,6 +215,7 @@ def init_events(bot, cli_flags):
             )
         else:
             log.exception(type(error).__name__, exc_info=error)
+        
 
     @bot.event
     async def on_message(message):
