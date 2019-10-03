ed740106f9a9e25a08d5286d7e38bfcebda0d5ab
Kennnyshiwa <kstjohn@kstjohn.us>
2019-10-03 14:31:01 -0400

    [info] fix broken image

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -349,7 +349,7 @@ class Core(commands.Cog, CoreLogic):
             embed.add_field(name=_("About this instance"), value=custom_info, inline=False)
         embed.add_field(name=_("About Red"), value=about, inline=False)
 
-        embed.set_image(url="https://photos.kstj.us/WobblySiennaSponge.png")
+        embed.set_image(url="https://i.imgur.com/BLClEw2.png")
 
         embed.set_footer(
             text=_("Bringing joy since 02 Jan 2016 (over {} days ago!)").format(days_since)
