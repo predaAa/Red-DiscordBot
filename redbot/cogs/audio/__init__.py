b292240bc59dfdbdcfc94b0c7d14c518d0256ddd
Kennnyshiwa <kstjohn@kstjohn.us>
2019-10-25 19:39:33 -0400

    Revert "sinbad magic"
    This reverts commit 7a2cbd92d0b455ed5fbc941aa061344947f8dec3.

--- redbot/cogs/audio/__init__.py
+++ redbot/cogs/audio/__init__.py
@@ -3,6 +3,7 @@ from redbot.core import commands
 from .audio import Audio
 
 
-def setup(bot: commands.Bot):
+async def setup(bot: commands.Bot):
     cog = Audio(bot)
+    await cog.initialize()
     bot.add_cog(cog)
